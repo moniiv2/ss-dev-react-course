82e6def3c3e3e669240a406de8d034c17b0ac66b
Monisola <bellomonisola867@gmail.com>
2025-09-10 10:51:27 +0100

    installed and applied tailwindcss to chatinput

--- chatbot-project/src/components/ChatInput.jsx
+++ chatbot-project/src/components/ChatInput.jsx
@@ -1,72 +1,78 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import { Chatbot } from 'supersimpledev';
-import Loader from '../assets/bean-eater-loader.gif'
+import Loader from '../assets/bean-eater-loader.gif';
 
 export function ChatInput({ chatMessages, setChatMessages }) {
-        const [inputText, setInputText] = useState('');
+  const [inputText, setInputText] = useState('');
 
-        function saveInputText(event) {
-          setInputText(event.target.value);
-        }
+  function saveInputText(event) {
+    setInputText(event.target.value);
+  }
 
-        async function sendMessage() {
-          const newChatMessages = [
-            ...chatMessages,
-            {
-              message: inputText,
-              sender: 'user',
-              id: crypto.randomUUID(),
-            },
-            {
-              message: (
-                <img src={Loader} height='40px' margin='15px' />
-              ),
-              sender: 'robot',
-              id: crypto.randomUUID(),
-            },
-          ];
+  async function sendMessage() {
+    const newChatMessages = [
+      ...chatMessages,
+      {
+        message: inputText,
+        sender: 'user',
+        id: crypto.randomUUID(),
+      },
+      {
+        message: <img src={Loader} height='40px' margin='15px' />,
+        sender: 'robot',
+        id: crypto.randomUUID(),
+      },
+    ];
 
-          setChatMessages(newChatMessages);
+    setChatMessages(newChatMessages);
 
-          setInputText('');
+    setInputText('');
 
-          const response = await Chatbot.getResponseAsync(inputText);
-          setChatMessages([
-            ...newChatMessages.slice(0, newChatMessages.length - 1),
-            {
-              message: response,
-              sender: 'robot',
-              id: crypto.randomUUID(),
-            },
-          ]);
-        }
+    const response = await Chatbot.getResponseAsync(inputText);
+    setChatMessages([
+      ...newChatMessages.slice(0, newChatMessages.length - 1),
+      {
+        message: response,
+        sender: 'robot',
+        id: crypto.randomUUID(),
+      },
+    ]);
+  }
 
-        function handleKeyDown(event) {
-          if (event.key === 'Enter') {
-            sendMessage();
-          } else if (event.key === 'Escape') {
-            setInputText('');
-          }
-        }
+  function handleKeyDown(event) {
+    if (event.key === 'Enter') {
+      sendMessage();
+    } else if (event.key === 'Escape') {
+      setInputText('');
+    }
+  }
 
-        function clearChat() {
-          setChatMessages([])
-        } 
+  function clearChat() {
+    setChatMessages([]);
+  }
 
-        return (
-          <div className='chat-input-container'>
-            <input
-              placeholder='Send a message to ChatBot'
-              size='35'
-              onChange={saveInputText}
-              value={inputText}
-              onKeyDown={handleKeyDown}
-              className='chat-input'
-            />
-            <button onClick={sendMessage} className='send-button'>
-              Send
-            </button>
-            <button onClick={clearChat}  className='clear-button'>Clear</button>
-          </div>
-        );
-      }
+  return (
+    <div className='flex mb-[50px]'>
+      <input
+        placeholder='Send a message to ChatBot'
+        size='35'
+        onChange={saveInputText}
+        value={inputText}
+        onKeyDown={handleKeyDown}
+        className='py-3 px-4 text-[15px] grow rounded-lg border border-black-100'
+      />
+      <button
+        onClick={sendMessage}
+        className='bg-[rgb(25,135,84)] text-white px-5 py-3 ml-[10px] rounded-[10px] text-[15px] cursor-pointer'
+      >
+        Send
+      </button>
+      <button
+        onClick={clearChat}
+        className='bg-[rgb(230,230,230)] ml-[10px] text-[15px] rounded-[10px] cursor-pointer px-5'
+      >
+        Clear
+      </button>
+    </div>
+  );
+}
