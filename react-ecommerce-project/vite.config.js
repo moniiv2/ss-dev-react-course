9ba0c10f4dc1e83a1ed5fd9f3b7b795f248146af
Monisola <bellomonisola867@gmail.com>
2025-09-11 15:19:13 +0100

    added server proxy  configfor /api and /images

--- react-ecommerce-project/vite.config.js
+++ react-ecommerce-project/vite.config.js
@@ -5,4 +5,15 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000'
+      },
+      '/images': {
+        target: 'http://localhost:3000'
+      }
+    }
+  }
 })
+//this is server proxy config
