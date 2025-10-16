185fc890e0345f3607be305394cb34a5f3b95e04
Monisola <bellomonisola867@gmail.com>
2025-10-16 13:27:57 +0100

    created new react typescript project

--- react-ecommerce-project/vite.config.js
+++ react-ecommerce-project/vite.config.js
@@ -4,7 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react({
+    babel: {
+    plugins: [['babel-plugin-react-compiler', { target: '19' }]],
+  },
+  }), tailwindcss()],
   server: {
     proxy: {
       '/api': {
